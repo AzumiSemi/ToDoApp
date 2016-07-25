c63d68c05650a382e74b742f1794811eab7c1cda
Tsubasa Yonenaga <shawn.winghills@gmail.com>
2016-07-25 09:04:34 +0900

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Tsubasa Yonenaga on 2016/07/25.
+//  Copyright © 2016年 Tsubasa Yonenaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Tsubasa Yonenaga on 2016/07/25.
+//  Copyright © 2016年 Tsubasa Yonenaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
